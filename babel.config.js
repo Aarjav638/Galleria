08ac36ea8ab6b85c1625974a07d3bf8795d16aa3
Aarjav Jain <anshjain638@gmail.com>
2024-09-30 20:51:51 +0530

    fixed build errros

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,12 @@
 module.exports = {
-  presets: [
-    'module:@react-native/babel-preset',
-    '@babel/preset-env', // For transforming ES6+ syntax
-    '@babel/preset-react', // For transforming JSX
-    '@babel/preset-typescript', // For transforming TypeScript,
-  ],
-  plugins: [
-    '@babel/plugin-transform-private-methods',
-    '@babel/plugin-transform-class-properties',
-    '@babel/plugin-transform-private-property-in-object',
-  ],
+  presets: ['module:@react-native/babel-preset'],
+  // '@babel/preset-env', // For transforming ES6+ syntax
+  // '@babel/preset-react', // For transforming JSX
+  // '@babel/preset-typescript', // For transforming TypeScript,
+  // ],
+  // plugins: [
+  //   '@babel/plugin-transform-private-methods',
+  //   '@babel/plugin-transform-class-properties',
+  //   '@babel/plugin-transform-private-property-in-object',
+  // ],
 };
