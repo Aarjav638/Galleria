b430ad0529298590db329d5469cdf3f822a14fd4
Aarjav Jain <anshjain638@gmail.com>
2024-09-30 20:34:37 +0530

    Tried to add Test JOb But it is failing so work in progress

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: [
+    'module:@react-native/babel-preset',
+    '@babel/preset-env', // For transforming ES6+ syntax
+    '@babel/preset-react', // For transforming JSX
+    '@babel/preset-typescript', // For transforming TypeScript,
+  ],
+  plugins: [
+    '@babel/plugin-transform-private-methods',
+    '@babel/plugin-transform-class-properties',
+    '@babel/plugin-transform-private-property-in-object',
+  ],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: 'react-native',
   transform: {
-    '^.+\\.tsx?$': 'ts-jest', // Using ts-jest for transforming TypeScript files
+    '^.+\\.[tj]sx?$': 'babel-jest', // Use babel-jest to transform TypeScript and JSX
   },
+  testEnvironment: 'jest-environment-jsdom', // Use jsdom for browser-like environment
   testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation))',
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@react-native-community|react-native-svg|react-native-reanimated)/)', // Ensure these packages are transformed
   ],
   moduleNameMapper: {
     '\\.(png|jpg|jpeg|svg)$': '<rootDir>/__mocks__/fileMock.js', // Mock static file imports like images
