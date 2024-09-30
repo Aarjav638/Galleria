ea79ee3657100f91caa0cad6b7e54a97a54e5679
Aarjav Jain <anshjain638@gmail.com>
2024-09-30 19:55:05 +0530

    included non js files in the jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   preset: 'react-native',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest', // For handling TypeScript files
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation))',
+  ],
+  moduleNameMapper: {
+    '\\.(png|jpg|jpeg|svg)$': '<rootDir>/__mocks__/fileMock.js', // Mock static file imports
+  },
+  setupFiles: ['<rootDir>/jest.setup.js'], // Optional: For additional setup before tests
 };
